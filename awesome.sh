76b3235382aab98d7cc0da60d0b82e709360362b
BB-8 <ichannel.jakpat@gmail.com>
2019-03-10 10:12:31 +0700

    build(scripts): add awesome:watch

--- awesome.sh
+++ awesome.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 cd packages/generator
+rm -rf src/generated
+mkdir -p src/generated/content
+cp -r ../../content src/generated/content
+
 yarn start
 cd ../..
 
 rm -rf content/generated
-cp -r packages/generator/src/generated/* .
+cp -r packages/generator/src/generated/README.md .
+cp -r packages/generator/src/generated/content/generated content/generated
