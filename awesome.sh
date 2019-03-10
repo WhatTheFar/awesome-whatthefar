18e65b5a85c3077730d7a05d1c6d6c50e1f7489c
BB-8 <ichannel.jakpat@gmail.com>
2019-03-10 13:58:23 +0700

    fix(scripts): correct awesome.sh

--- awesome.sh
+++ awesome.sh
@@ -3,7 +3,7 @@
 cd packages/generator
 rm -rf src/generated
 mkdir -p src/generated/content
-cp -r ../../content src/generated/content
+cp -r ../../content/* src/generated/content
 
 yarn start
 cd ../..
