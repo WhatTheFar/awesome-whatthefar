4372dd5315845c8ba510a7c4c73930e7eccc8265
BB-8 <ichannel.jakpat@gmail.com>
2019-03-20 22:23:27 +0700

    feat: remove generated contents

--- awesome.sh
+++ awesome.sh
@@ -9,11 +9,11 @@ rm -rf src/generated/content/generated
 yarn start
 cd ../..
 
-rm -rf content/generated
-cp -r packages/generator/src/generated/README.md .
-cp -r packages/generator/src/generated/content/generated content/generated
+# rm -rf content/generated
+# cp -r packages/generator/src/generated/README.md .
+# cp -r packages/generator/src/generated/content/generated content/generated
 
 rm -rf temp
 mkdir -p temp
-cp -r content temp/content
-cp README.md temp
+cp -r packages/generator/src/generated/README.md temp
+cp -r packages/generator/src/generated/content temp/content
