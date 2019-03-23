7ccc38f9831a2fc9c77ed497e26b02b10264da91
BB-8 <ichannel.jakpat@gmail.com>
2019-03-23 09:44:47 +0700

    fix(parser): add NEW_LINE after MarkdownImage

--- packages/parser/src/markdown/item/image.ts
+++ packages/parser/src/markdown/item/image.ts
@@ -1,8 +1,9 @@
+import { NEW_LINE } from './../constant';
 import { MarkdownImage, MarkdownPageContext } from './../types';
 
 export function parseMarkdownImage(
 	{ altText, path, title }: MarkdownImage,
 	{ helper }: MarkdownPageContext
 ): string {
-	return helper.createMarkdownImage(altText, path, title);
+	return helper.createMarkdownImage(altText, path, title) + NEW_LINE;
 }
