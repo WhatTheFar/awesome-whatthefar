31be0f12d74791521596b1d170191a9c42cdce23
WhatTheFar <jakpat.m@gmail.com>
2021-01-03 16:58:30 +0700

    build(gen): setup lib and build scripts

--- packages/generator/src/index.ts
+++ packages/generator/src/index.ts
@@ -4,11 +4,15 @@ import { generateMarkdownFile } from '@awesome-whatthefar/parser';
 import _ from 'lodash';
 import { createReadmePage } from './input/readme';
 
-(async () => {
+async function main(): Promise<void> {
 	const label = '✨  Done';
 	console.time(label);
 
 	await generateMarkdownFile(await createReadmePage());
 
 	console.timeEnd(label);
-})();
+}
+
+if (require.main === module) {
+	main();
+}
